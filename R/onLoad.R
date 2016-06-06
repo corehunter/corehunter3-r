f713f9586c56c063456ccff234842d48c75a6f58
Herman De Beukelaer <herman.debeukelaer@gmail.com>
2016-06-06 14:22:45 +0200

    resolved CRAN check notes

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,8 +1,4 @@
 #' @import rJava
 .onLoad <- function(libname, pkgname) {
-  print("--- onLoad {")
   .jpackage(pkgname, lib.loc = libname)
-  print(dir(recursive = T))
-  print(.jclassPath())
-  print("--- onLoad }")
 }
