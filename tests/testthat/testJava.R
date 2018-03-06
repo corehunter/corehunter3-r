0ee7895456b6c33a951e4a9cc1d4d20c74f7b6fa
Herman De Beukelaer <herman.debeukelaer@conundra.eu>
2018-03-06 20:38:42 +0100

    fix mockr import

--- tests/testthat/testJava.R
+++ tests/testthat/testJava.R
@@ -4,7 +4,6 @@ source("testUtils.R")
 context("Java")
 ###############
 
-#' @importFrom mockr with_mock
 test_that("version correctly parsed for JDK 8", {
   # mock version string function
   with_mock(
@@ -13,7 +12,6 @@ test_that("version correctly parsed for JDK 8", {
   )
 })
 
-#' @importFrom mockr with_mock
 test_that("version correctly parsed for JDK 9", {
   # mock version string function
   version.strings <- c(
